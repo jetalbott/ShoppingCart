79a6267190bf101a1bad66b9f66d70ac15806cb7
root <root@ubuntu.(none)>
2013-11-13 22:26:42 -0800

    initial commit

---
+++ Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $shopping_cart = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'mypass',
+		'database' => 'shopping_cart',
+	);
+}
